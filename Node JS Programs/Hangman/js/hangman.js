545f7bb023456c414623b7af2b63ca8cf1ed9bbb
Alfie Froude <alfiefroude111@gmail.com>
2021-03-04 10:18:59 +0000

    Initial File Transfer

---
+++ Node JS Programs/Hangman/js/hangman.js
@@ -0,0 +1,7 @@
+const my_array = ['Alfie', 'Harry'];
+
+my_array.forEach(
+    function(twin){
+        console.log(twin)
+    }
+)
---
+++ Node JS Programs/Practice JS/practice.js
@@ -0,0 +1,1 @@
+const alphArray = ['abcdefghijklmnopqrstuvwxyz'];
